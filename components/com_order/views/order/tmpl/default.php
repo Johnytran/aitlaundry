126a5774ed189a3f99bf7c685d67e20a57967f9e
tuananhasp <tuananhasp@gmail.com>
2017-05-22 12:10:01 -1000

    order view

--- components/com_order/views/order/tmpl/default.php
+++ components/com_order/views/order/tmpl/default.php
@@ -11,63 +11,71 @@ defined('_JEXEC') or die;
 
 
 ?>
-
-<div class="item_fields">
-
-	<table class="table">
-		
-
-		<tr>
-			<th><?php echo JText::_('COM_ORDER_FORM_LBL_ORDER_ORDERNOTE'); ?></th>
-			<td><?php echo nl2br($this->item->ordernote); ?></td>
-		</tr>
-
-		<tr>
-			<th><?php echo JText::_('COM_ORDER_FORM_LBL_ORDER_COMBOID'); ?></th>
-			<td><?php echo $this->item->comboid; ?></td>
-		</tr>
-
-		<tr>
-			<th><?php echo JText::_('COM_ORDER_FORM_LBL_ORDER_PAYMENTID'); ?></th>
-			<td><?php echo $this->item->paymentid; ?></td>
-		</tr>
-
-		<tr>
-			<th><?php echo JText::_('COM_ORDER_FORM_LBL_ORDER_DATETIMECREATED'); ?></th>
-			<td><?php echo $this->item->datetimecreated; ?></td>
-		</tr>
-
-		<tr>
-			<th><?php echo JText::_('COM_ORDER_FORM_LBL_ORDER_ADDRESSPICKUP'); ?></th>
-			<td><?php echo $this->item->addresspickup; ?></td>
-		</tr>
-
-		<tr>
-			<th><?php echo JText::_('COM_ORDER_FORM_LBL_ORDER_DATE_TIMEPICKUP'); ?></th>
-			<td><?php echo $this->item->date_timepickup; ?></td>
-		</tr>
-
-		<tr>
-			<th><?php echo JText::_('COM_ORDER_FORM_LBL_ORDER_ADDRESSDELIVERY'); ?></th>
-			<td><?php echo $this->item->addressdelivery; ?></td>
-		</tr>
-
-		<tr>
-			<th><?php echo JText::_('COM_ORDER_FORM_LBL_ORDER_DATE_TIMEDELIVERY'); ?></th>
-			<td><?php echo $this->item->date_timedelivery; ?></td>
-		</tr>
-
-		<tr>
-			<th><?php echo JText::_('COM_ORDER_FORM_LBL_ORDER_DELIVERYNOTE'); ?></th>
-			<td><?php echo nl2br($this->item->deliverynote); ?></td>
-		</tr>
-
-		<tr>
-			<th><?php echo JText::_('COM_ORDER_FORM_LBL_ORDER_USERID'); ?></th>
-			<td><?php echo $this->item->userid; ?></td>
-		</tr>
-
-	</table>
-
-</div>
-
+<form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+  </div>
+  <div class="form-group">
+    <label for="exampleSelect1">Example select</label>
+    <select class="form-control" id="exampleSelect1">
+      <option>1</option>
+      <option>2</option>
+      <option>3</option>
+      <option>4</option>
+      <option>5</option>
+    </select>
+  </div>
+  <div class="form-group">
+    <label for="exampleSelect2">Example multiple select</label>
+    <select multiple class="form-control" id="exampleSelect2">
+      <option>1</option>
+      <option>2</option>
+      <option>3</option>
+      <option>4</option>
+      <option>5</option>
+    </select>
+  </div>
+  <div class="form-group">
+    <label for="exampleTextarea">Example textarea</label>
+    <textarea class="form-control" id="exampleTextarea" rows="3"></textarea>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputFile">File input</label>
+    <input type="file" class="form-control-file" id="exampleInputFile" aria-describedby="fileHelp">
+    <small id="fileHelp" class="form-text text-muted">This is some placeholder block-level help text for the above input. It's a bit lighter and easily wraps to a new line.</small>
+  </div>
+  <fieldset class="form-group">
+    <legend>Radio buttons</legend>
+    <div class="form-check">
+      <label class="form-check-label">
+        <input type="radio" class="form-check-input" name="optionsRadios" id="optionsRadios1" value="option1" checked>
+        Option one is this and that&mdash;be sure to include why it's great
+      </label>
+    </div>
+    <div class="form-check">
+    <label class="form-check-label">
+        <input type="radio" class="form-check-input" name="optionsRadios" id="optionsRadios2" value="option2">
+        Option two can be something else and selecting it will deselect option one
+      </label>
+    </div>
+    <div class="form-check disabled">
+    <label class="form-check-label">
+        <input type="radio" class="form-check-input" name="optionsRadios" id="optionsRadios3" value="option3" disabled>
+        Option three is disabled
+      </label>
+    </div>
+  </fieldset>
+  <div class="form-check">
+    <label class="form-check-label">
+      <input type="checkbox" class="form-check-input">
+      Check me out
+    </label>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
