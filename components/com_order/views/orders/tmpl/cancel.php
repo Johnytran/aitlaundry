fe094d2e11fd9e893cc0ba03a3b4cbcbd58fbc4f
tuananhasp <tuananhasp@gmail.com>
2017-05-24 16:37:13 -1000

    add

---
+++ components/com_order/views/orders/tmpl/cancel.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @version    CVS: 1.0.0
+ * @package    Com_Order
+ * @author     AIT Laundry <aitlaundry@gmail.com>
+ * @copyright  2017 AIT Laundry
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
+// No direct access
+defined('_JEXEC') or die;
+$document = JFactory::getDocument();
+$document->setTitle('Cancel Order');
+?>
+<h3>The order is canceled!</h3>
